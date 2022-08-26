3b85dd3e2f0d90faf3418c6bd0b18f29e9260b12
Administrator <jiyeonyee0312@gmail.com>
2022-08-26 20:20:40 +0900

    초기 커밋.

---
+++ Simple Clicker/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Simple_Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblTickPoint = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1Add = new System.Windows.Forms.Button();
+            this.btn3Add = new System.Windows.Forms.Button();
+            this.btn50Add = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblTotal);
+            this.groupBox1.Controls.Add(this.lblTickPoint);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(410, 141);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "점수";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lblTotal.Location = new System.Drawing.Point(6, 49);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(398, 78);
+            this.lblTotal.TabIndex = 2;
+            this.lblTotal.Text = "0";
+            this.lblTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTickPoint
+            // 
+            this.lblTickPoint.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTickPoint.Location = new System.Drawing.Point(83, 14);
+            this.lblTickPoint.Name = "lblTickPoint";
+            this.lblTickPoint.Size = new System.Drawing.Size(327, 23);
+            this.lblTickPoint.TabIndex = 1;
+            this.lblTickPoint.Text = "0";
+            this.lblTickPoint.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tick Point : ";
+            // 
+            // btn1Add
+            // 
+            this.btn1Add.Location = new System.Drawing.Point(12, 159);
+            this.btn1Add.Name = "btn1Add";
+            this.btn1Add.Size = new System.Drawing.Size(204, 23);
+            this.btn1Add.TabIndex = 1;
+            this.btn1Add.Text = "+1";
+            this.btn1Add.UseVisualStyleBackColor = true;
+            this.btn1Add.Click += new System.EventHandler(this.btn1Add_Click);
+            // 
+            // btn3Add
+            // 
+            this.btn3Add.Location = new System.Drawing.Point(12, 188);
+            this.btn3Add.Name = "btn3Add";
+            this.btn3Add.Size = new System.Drawing.Size(204, 23);
+            this.btn3Add.TabIndex = 2;
+            this.btn3Add.Text = "+2";
+            this.btn3Add.UseVisualStyleBackColor = true;
+            // 
+            // btn50Add
+            // 
+            this.btn50Add.Location = new System.Drawing.Point(12, 217);
+            this.btn50Add.Name = "btn50Add";
+            this.btn50Add.Size = new System.Drawing.Size(204, 23);
+            this.btn50Add.TabIndex = 3;
+            this.btn50Add.Text = "+50";
+            this.btn50Add.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(222, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "(100)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(222, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "(500)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(222, 221);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "(5000)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 256);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn50Add);
+            this.Controls.Add(this.btn3Add);
+            this.Controls.Add(this.btn1Add);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label lblTotal;
+        private Label lblTickPoint;
+        private Label label1;
+        private Button btn1Add;
+        private Button btn3Add;
+        private Button btn50Add;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+    }
+}
